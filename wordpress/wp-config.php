d5901eaa4f19a55bc8334aac71a0176433a63b25
CodeurIvoirien225 <koneelabass1@gmail.com>
2025-10-18 19:11:41 +0000

    Ajout du dossier wordpress

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'avocat_client_db' );
+define( 'DB_NAME', 'pgroupeb_JustisConnect' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'SYSCOA90' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'localhost:3306' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
